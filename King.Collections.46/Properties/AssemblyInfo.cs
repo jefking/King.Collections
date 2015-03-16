861e101af0138a8926192c66e79fe000c45d8437
Jef King <jef@agilebusinesscloud.com>
2015-03-16 12:55:15 -0700

    updated

---
+++ King.Collections.46/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Collections.46")]
+[assembly: AssemblyDescription("Additional collections, and sorting for .NET.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Collections")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9175af63-6c3e-4ec7-9e0e-32aa1e2e3431")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
