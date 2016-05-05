3868fad0e5881cad4e7834e0e58345bbe863d6cf
Jef King <jefkin@microsoft.com>
2016-05-04 20:22:29 -0700

    more bnetter

--- King.Collections.Test.Unit/Program.cs
+++ King.Collections.Test.Unit/Program.cs
@@ -3,12 +3,22 @@
     using NUnit.Common;
     using NUnitLite;
     using System;
-
+    using System.Reflection;
     public class Program
     {
         public static int Main(string[] args)
         {
-            return 0;
+            AutoRun ar;
+
+#if DNX20
+            ar new AutoRun().Execute(args);
+#else
+            ar new AutoRun(typeof(Program).GetTypeInfo().Assembly);
+#endif
+            var writer = new ExtendedTextWrapper(Console.Out);
+            var reader = Console.In;
+
+            return ar.Execute(args, writer, reader);
         }
     }
 }
