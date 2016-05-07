2d843796c1bc3a6aca2f07c24c8feccd77f979e7
Jef King <jefkin@microsoft.com>
2016-05-07 15:54:27 -0700

    more closer

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,7 +1,7 @@
 CD .\artifacts\bin\King.Collections.Test.Unit\Release\app
 
 dnvm use 1.0.0-rc1-update1 -r clr -a x64
-dnu restore -f .
+dnu restore -f . ../../../King.Collections/Release
 .\King.Collections.Test.Unit.cmd
 
 CD ../../../../../
