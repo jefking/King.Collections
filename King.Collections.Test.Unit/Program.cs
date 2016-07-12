a7543cf09919939d43f95e31f6a8c2df6c8eb093
Jef King <jefkin@microsoft.com>
2016-07-12 13:52:55 -0700

    read baby read

--- King.Collections.Test.Unit/Program.cs
+++ King.Collections.Test.Unit/Program.cs
@@ -11,6 +11,8 @@
         {
             var writter = new ExtendedTextWrapper(Console.Out);
             new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args, writter, Console.In);
+
+            Console.Read();
         }
     }
 }
